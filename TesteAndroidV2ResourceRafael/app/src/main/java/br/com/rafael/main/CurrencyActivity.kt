8bcfa34625d1d91f3ffbe0a5b8006e1c37ef1d74
macedoraf <macedo.rafanazareth@gmail.com>
2018-10-27 14:21:31 -0300

    Layout da Currency Activity criado porém não foi totalmente finalizado, são necessários alguns ajustes se será usado uma toolbar ou algum componente diferente para mostrar os dados do usuario e o botão de logout

--- TesteAndroidV2ResourceRafael/app/src/main/java/br/com/rafael/main/CurrencyActivity.kt
+++ TesteAndroidV2ResourceRafael/app/src/main/java/br/com/rafael/main/CurrencyActivity.kt
@@ -1,7 +1,8 @@
 package br.com.rafael.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import br.com.rafael.testeandroidv2resourcerafael.R
 
 class CurrencyActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class CurrencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency)
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.menu_currency)
+
+
     }
+
 }
--- TesteAndroidV2ResourceRafael/app/src/main/java/br/com/rafael/main/LoginActivity.kt
+++ TesteAndroidV2ResourceRafael/app/src/main/java/br/com/rafael/main/LoginActivity.kt
@@ -1,5 +1,6 @@
 package br.com.rafael.main
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.rafael.testeandroidv2resourcerafael.R
@@ -9,5 +10,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        intent  = Intent(this,CurrencyActivity::class.java)
+        startActivity(intent)
     }
 }
