a2f429b9516c37e2734e765a14f3778b8319b323
macedoraf <macedo.rafanazareth@gmail.com>
2018-10-27 05:33:46 -0300

    Correção de pequenos bugs de arquivos e criação dos arquivos de da tela de currency

---
+++ TesteAndroidV2ResourceRafael/app/src/main/java/br/com/rafael/main/CurrencyActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rafael.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rafael.testeandroidv2resourcerafael.R
+
+class CurrencyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency)
+    }
+}
